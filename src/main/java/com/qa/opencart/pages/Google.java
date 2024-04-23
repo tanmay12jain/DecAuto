3da6abab175021ba0d5fb96f8719e360e3148af6
tanmay12jain <133835878+tanmay12jain@users.noreply.github.com>
2024-04-23 16:03:54 +0530

    Update Google.java

--- src/main/java/com/qa/opencart/pages/Google.java
+++ src/main/java/com/qa/opencart/pages/Google.java
@@ -2,12 +2,12 @@ package com.qa.opencart.pages;
 
 public class Google {
 
-
 	public void login() {
 		System.out.println("Tanmay--- login");
-	System.out.println("done");
+		System.out.println("done");
 	}
-	public void forgotpssword() {
-		System.out.println("forgotpssword");
+
+	public void forgot() {
+		System.out.println("forgot");
 	}
 }

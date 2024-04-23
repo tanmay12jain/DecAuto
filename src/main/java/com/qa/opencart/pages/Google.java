2cba692a48a1b8dcb999ed2c5f360cefb914bdba
tanmay12jain <133835878+tanmay12jain@users.noreply.github.com>
2024-04-23 15:55:08 +0530

    Update Google.java

--- src/main/java/com/qa/opencart/pages/Google.java
+++ src/main/java/com/qa/opencart/pages/Google.java
@@ -5,6 +5,8 @@ public class Google {
 
 	public void login() {
 		System.out.println("Tanmay--- login");
-		
 	System.out.println("done");}
+	public void registration() {
+		System.out.println("register");
+	}
 }
